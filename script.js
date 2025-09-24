48bbb3c80aadb63a5533204f3d0b65a49615acd1
ankitkumar0008 <96786356+ankitkumar0008@users.noreply.github.com>
2025-09-24 10:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let items = document.querySelectorAll(".grid-item");
+let changebutton  = document.getElementById("change_button");
+
+changebutton.addEventListener("click",()=>{
+	items.forEach((cell)=>{
+		cell.style.backgroundColor = "transparent"
+	})
+	let cell = document.getElementById("block_id").value;
+	let color = document.getElementById("colour_id").value;
+
+	let getcell = document.getElementById(`${cell}`);
+	getcell.style.backgroundColor = color;
+})
+
+let resetbutton = document.getElementById("Reset");
+
+resetbutton.addEventListener("click",()=>{
+	items.forEach((cell)=>{
+		cell.style.backgroundColor = "transparent"
+	})
+})
